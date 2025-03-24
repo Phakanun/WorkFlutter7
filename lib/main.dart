b08292f76e091c42baf56bf173d7c8c630915b1c
Phakanun <s6504062630201@email.kmutnb.ac.th>
2025-03-25 04:42:41 +0700

    hw

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/my_homepage_sharedpref.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(color: Color.fromARGB(255, 58, 72, 117)),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 152, 173, 231),
+        ),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(title: 'Homepage'),
+    );
+  }
+}
